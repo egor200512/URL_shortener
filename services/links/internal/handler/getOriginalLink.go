1f6119ad3889568fa81388be47e2d6d2d8d482eb
Egor <egoor200561@gmail.com>
2026-01-19 17:46:25 +0300

    get original l

---
+++ services/links/internal/handler/getOriginalLink.go
@@ -0,0 +1,27 @@
+package handler
+
+import (
+	"context"
+	"fmt"
+
+	desc "github.com/egor200512/URL_shortener/shared/gen/links"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (handler *LinksHandler) GetOriginalLink(ctx context.Context, in *desc.GetOriginalLinkRequest) (*desc.GetOriginalLinkResponse, error) {
+	if len(in.ShortLink) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "short link is required")
+	}
+
+	l, err := handler.linksService.GetLinkInfo(ctx, in.ShortLink)
+	if err != nil {
+		return nil, status.Error(codes.Internal, fmt.Errorf("failed to get link info: %s", err.Error()).Error())
+	}
+
+	if l == nil {
+		return nil, status.Error(codes.NotFound, fmt.Errorf("link for %s doesn't exist", in.ShortLink).Error())
+	}
+
+	return &desc.GetOriginalLinkResponse{OriginalUrl: l.OriginalLink}, nil
+}
