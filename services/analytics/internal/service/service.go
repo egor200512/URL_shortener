d75c3a5992b7f381510ed04506dfb74a9b0f381c
Egor <egoor200561@gmail.com>
2026-01-29 15:56:35 +0300

    clean up

--- services/analytics/internal/service/service.go
+++ services/analytics/internal/service/service.go
@@ -12,7 +12,6 @@ import (
 type IAnalyticsService interface {
 	GetEvents(ctx context.Context, req *desc.GetEventsRequest) (*desc.GetEventsResponse, error)
 	Health(ctx context.Context, req *emptypb.Empty) (*emptypb.Empty, error)
-	Metrics(ctx context.Context, req *emptypb.Empty) (*emptypb.Empty, error)
 
 	HandleMessage(ctx context.Context, msg *gonats.Msg) error
 }
