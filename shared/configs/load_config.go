e73a7a9954bb224a2553a340e7ced2fd8142e91d
Egor <egoor200561@gmail.com>
2026-05-25 18:24:17 +0300

    local run

--- shared/configs/load_config.go
+++ shared/configs/load_config.go
@@ -2,12 +2,40 @@ package configs
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/lpernett/godotenv"
 )
 
 func LoadConfig(path string) {
 	if err := godotenv.Load(path); err != nil {
-		panic(fmt.Errorf("failed to load config: %s", err.Error()))
+		fallbackPath, fallbackErr := findEnvFile()
+		if fallbackErr != nil {
+			panic(fmt.Errorf("failed to load config: %s", err.Error()))
+		}
+		if err := godotenv.Load(fallbackPath); err != nil {
+			panic(fmt.Errorf("failed to load config: %s", err.Error()))
+		}
+	}
+}
+
+func findEnvFile() (string, error) {
+	dir, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+
+	for {
+		path := filepath.Join(dir, ".env")
+		if _, err := os.Stat(path); err == nil {
+			return path, nil
+		}
+
+		parent := filepath.Dir(dir)
+		if parent == dir {
+			return "", os.ErrNotExist
+		}
+		dir = parent
 	}
 }
